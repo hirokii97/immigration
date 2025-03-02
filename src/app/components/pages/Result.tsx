44ec875ef7394a7f8070513b969d0bc6383685e1
hirokii97 <basket100713hiroki.ch@gmail.com>
2025-03-02 15:11:25 +0900

    検索ボタンを追加

--- src/app/components/pages/Result.tsx
+++ src/app/components/pages/Result.tsx
@@ -3,13 +3,9 @@
 import Card from "@/app/components/ui/Card";
 import { searchWordAtom } from "@/app/utils/atom";
 import { useAtom } from "jotai";
-import React, { useEffect } from "react";
 
 export default function Result() {
   const [searchWord] = useAtom<string>(searchWordAtom);
-  useEffect(() => {
-    console.log(searchWord);
-  }, [searchWord]);
 
   return (
     <div>

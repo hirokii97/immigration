97da4702766d83508ac754936ab57e00eb03ce1e
hirokii97 <basket100713hiroki.ch@gmail.com>
2025-03-02 13:37:55 +0900

    検索結果を状態管理する

--- src/app/components/pages/Result.tsx
+++ src/app/components/pages/Result.tsx
@@ -1,10 +1,19 @@
-import Card from '@/app/components/ui/Card'
-import React from 'react'
+"use client";
+
+import Card from "@/app/components/ui/Card";
+import { searchWordAtom } from "@/app/utils/atom";
+import { useAtom } from "jotai";
+import React, { useEffect } from "react";
 
 export default function Result() {
+  const [serchWord] = useAtom<string>(searchWordAtom);
+  useEffect(() => {
+    console.log(serchWord);
+  }, [serchWord]);
+
   return (
     <div>
-      <Card/>
+      <Card />
     </div>
-  )
+  );
 }
--- src/app/components/ui/SearchInput.tsx
+++ src/app/components/ui/SearchInput.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+"use client";
+
+import { searchWordAtom } from "@/app/utils/atom";
+import { useAtom } from "jotai";
+import React, { useEffect } from "react";
 
 export default function SearchInput() {
+  const [serchWord, setSearchWord] = useAtom<string>(searchWordAtom);
+  useEffect(() => {
+    console.log(serchWord);
+  }, [serchWord]);
   return (
     <form className="max-w-md mx-auto">
-      <label
-        className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-      >
+      <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
         Search
       </label>
       <div className="relative">
@@ -19,9 +25,6 @@ export default function SearchInput() {
           >
             <path
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
               d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
             />
           </svg>
@@ -30,8 +33,10 @@ export default function SearchInput() {
           type="search"
           id="default-search"
           className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="都道府県・市長村"
+          placeholder="都道府県・市町村"
           required
+          value={serchWord}
+          onChange={(e) => setSearchWord(e.target.value)}
         />
         {/* <button
           type="submit"

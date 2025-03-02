eca1b529ce0fdf597a45ee502d8a018dda5bc011
hirokii97 <basket100713hiroki.ch@gmail.com>
2025-03-02 11:56:49 +0900

    tailwindの導入

---
+++ src/app/components/ui/Card.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Card() {
+  return (
+    <div className='text-red-50 max-w-sm bg-wthite border-gray-200 shadow-sm'>
+      検索結果
+    </div>
+  )
+}

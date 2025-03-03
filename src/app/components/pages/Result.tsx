8d3422377e601e1f7a6c7d915c845d9fcdadc5c2
hirokii97 <basket100713hiroki.ch@gmail.com>
2025-03-03 23:24:43 +0900

    ワーケーションデータをJSONで管理した場合

--- src/app/components/pages/Result.tsx
+++ src/app/components/pages/Result.tsx
@@ -1,16 +1,34 @@
 "use client";
 
 import Card from "@/app/components/ui/Card";
-import { searchWordAtom } from "@/app/utils/atom";
+import Margin from "@/app/components/ui/Margin";
+import { getResultData } from "@/app/services/getResultData";
+import { CityType } from "@/app/types/city";
+import { resultsAtom, searchWordAtom } from "@/app/utils/atom";
 import { useAtom } from "jotai";
+import { useEffect } from "react";
 
 export default function Result() {
   const [searchWord] = useAtom<string>(searchWordAtom);
+  const [results, setResult] = useAtom<CityType[]>(resultsAtom);
 
+  //   検索結果を出力する関数
+  useEffect(() => {
+    const results = getResultData(searchWord);
+    setResult(results);
+  }, [searchWord]);
   return (
-    <div>
-      <Card />
-      <Card />
+    <div className="flex flex-wrap gap-6">
+      {results.map((result) => (
+        <div key={result.id}>
+          <Card
+            key={result.id}
+            prefecture={result.prefecture}
+            municipality={result.municipality}
+            description={result.description}
+          />
+        </div>
+      ))}
     </div>
   );
 }
--- src/app/components/ui/Card.tsx
+++ src/app/components/ui/Card.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import Image from "next/image";
 
-export default function Card() {
+export default function Card(props: {
+  prefecture: string;
+  municipality: string;
+  description: string;
+}) {
+  const { prefecture, municipality, description } = props;
   return (
     <a className="block p-6 rounded-lg max-w-sm bg-wthite border-grey-200 shadow-sm dark:bg-gray-800">
-      <p className="text-xl font-semibold mb-4">北海道****市</p>
+      <h3 className="text-xl font-semibold mb-1">{prefecture}</h3>
+      <p className="text-sm font-semibold mb-4">{municipality}</p>
       <div className="image">
-        <Image className="object-cover rounded-xl w-full" src="/dummy1.jpg" alt="image" width={300} height={200} />
+        <Image
+          className="object-cover rounded-xl w-full"
+          src="/dummy1.jpg"
+          alt="image"
+          width={300}
+          height={200}
+        />
       </div>
-      <p className="text-sm mt-4">
-      この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、
-      </p>
+      <p className="text-sm mt-4">{description}</p>
     </a>
   );
 }

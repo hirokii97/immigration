b4ad2ec72bd3201807d1529696b428e34d476f2f
hirokii97 <basket100713hiroki.ch@gmail.com>
2025-03-15 17:46:20 +0900

    テーブルの修正・MAP実装

--- src/app/city/[cityId]/_componets/Table.tsx
+++ src/app/city/[cityId]/_componets/Table.tsx
@@ -4,10 +4,10 @@ import React from "react";
 export default function Table(props: { city: CityType }) {
   const { city } = props;
   return (
-    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+    <div className="relative overflow-x-auto shadow-md ">
+      <table className="border border-color-gray w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <tbody>
-          <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
+          <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border border-color-gray">
             <th
               scope="row"
               className="w-32 px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
@@ -16,7 +16,7 @@ export default function Table(props: { city: CityType }) {
             </th>
             <td className="px-6 py-4">{city.prefecture + city.municipality}</td>
           </tr>
-          <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
+          <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border border-color-gray">
             <th
               scope="row"
               className="w-32 px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
@@ -25,7 +25,7 @@ export default function Table(props: { city: CityType }) {
             </th>
             <td className="px-6 py-4">{city.averageTemperature}度</td>
           </tr>
-          <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
+          <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border border-color-gray">
             <th
               scope="row"
               className="w-32 px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
@@ -36,7 +36,7 @@ export default function Table(props: { city: CityType }) {
               {city.nightlyCost.toLocaleString()}円〜
             </td>
           </tr>
-          <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
+          <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border border-color-gray">
             <th
               scope="row"
               className="w-32 px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
@@ -56,6 +56,21 @@ export default function Table(props: { city: CityType }) {
             </th>
             <td className="px-6 py-4">
               {city.displayAvailable ? "あり" : "なし"}
+            </td>
+          </tr>
+          <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border border-color-gray">
+            <th
+              scope="row"
+              className="w-32 px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              MAP
+            </th>
+            <td className="px-6 py-4">
+              <iframe
+                src={`http://maps.google.co.jp/maps?q=${city.municipality}&output=embed&t=m&z=10&hl=ja`}
+                width="600"
+                height="450"
+              ></iframe>
             </td>
           </tr>
         </tbody>

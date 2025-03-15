27c22cc226e8b6444b67658a3eb35e6a3be76669
hirokii97 <basket100713hiroki.ch@gmail.com>
2025-03-15 18:15:27 +0900

    ひらがなも検索機能に含める

--- src/app/services/filterData.ts
+++ src/app/services/filterData.ts
@@ -2,7 +2,12 @@ import city from "@/app/data/city.json";
 
 export const filterData = (searchWord: string) => {
   const filteredData = city.filter((city) => {
-    return city.prefecture.includes(searchWord) || city.municipality.includes(searchWord);
+    return (
+      city.prefecture_hiragana.includes(searchWord) ||
+      city.municipality_hiragana.includes(searchWord) ||
+      city.prefecture.includes(searchWord) ||
+      city.municipality.includes(searchWord)
+    );
   });
   return filteredData;
 };
